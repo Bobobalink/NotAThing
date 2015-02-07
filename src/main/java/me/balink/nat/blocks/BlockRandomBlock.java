56e0a3ec2f00a80948212e7cea258358b61a1ed7
Grey <bobobalink@gmail.com>
2015-02-07 00:39:05 -0500

    Fixed BlockRandomBlock's formatting

--- src/main/java/me/balink/nat/blocks/BlockRandomBlock.java
+++ src/main/java/me/balink/nat/blocks/BlockRandomBlock.java
@@ -1,34 +1,31 @@
 package me.balink.nat.blocks;
 
-import me.balink.nat.init.ModBlocks;
 import me.balink.nat.init.ModItems;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.util.BlockPos;
 import net.minecraft.world.World;
 
 import java.util.Random;
 
-public class BlockRandomBlock extends BlockNAT{
-    
+public class BlockRandomBlock extends BlockNAT {
+
     public BlockRandomBlock() {
         super(Material.wood, "RandomBlock");
         this.setHardness(1.0f);
         this.setLightLevel(15.0f);
     }
-    
+
     @Override
-    public boolean canPlaceBlockAt(World world,  BlockPos pos) {
-        return  super.canPlaceBlockAt(world, pos)
-                && !(world.getBlockState(pos.down()).getBlock() instanceof BlockNAT)
-                && !(world.getBlockState(pos.up()).getBlock() instanceof BlockNAT)
-                && !(world.getBlockState(pos.east()).getBlock() instanceof BlockNAT)
-                && !(world.getBlockState(pos.west()).getBlock() instanceof BlockNAT)
-                && !(world.getBlockState(pos.north()).getBlock() instanceof BlockNAT)
-                && !(world.getBlockState(pos.south()).getBlock() instanceof BlockNAT)
-                ;
+    public boolean canPlaceBlockAt(World world, BlockPos pos) {
+        return super.canPlaceBlockAt(world, pos)
+               && !(world.getBlockState(pos.down()).getBlock() instanceof BlockNAT)
+               && !(world.getBlockState(pos.up()).getBlock() instanceof BlockNAT)
+               && !(world.getBlockState(pos.east()).getBlock() instanceof BlockNAT)
+               && !(world.getBlockState(pos.west()).getBlock() instanceof BlockNAT)
+               && !(world.getBlockState(pos.north()).getBlock() instanceof BlockNAT)
+               && !(world.getBlockState(pos.south()).getBlock() instanceof BlockNAT);
     }
 
     @Override
