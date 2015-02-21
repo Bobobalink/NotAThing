6ddd687da5826c68d36c5ae5adc6860540e54ebc
Grey <bobobalink@gmail.com>
2015-02-21 02:38:28 -0500

    played with RandomBlock

--- src/main/java/me/balink/nat/blocks/BlockRandomBlock.java
+++ src/main/java/me/balink/nat/blocks/BlockRandomBlock.java
@@ -1,10 +1,14 @@
 package me.balink.nat.blocks;
 
 import me.balink.nat.init.ModItems;
+import me.balink.nat.util.LogHelper;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.world.World;
 
 import java.util.Random;
@@ -33,5 +37,13 @@ public class BlockRandomBlock extends BlockNAT {
         if(rand.nextBoolean())
             return super.getItemDropped(state, rand, fortune);
         return ModItems.randomThing;
+    }
+
+    @Override
+    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumFacing side, float hitX, float hitY, float hitZ) {
+        ItemStack toPlace = playerIn.inventory.getCurrentItem();
+        if(toPlace != null) toPlace.onItemUse(playerIn, worldIn, pos.add(1, 1, 1), EnumFacing.DOWN, 0.5f, 0.5f, 0.5f);
+        LogHelper.info(pos);
+        return true;
     }
 }
